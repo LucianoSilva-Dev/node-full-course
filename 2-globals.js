3ed1f311f826d188e12a3f0056fadb95793cf1bf
LucianoDev <lucianonascimento2626@gmail.com>
2024-06-11 16:49:50 -0300

    variaveis globais

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+//GLOBALS
+
+// __dirname - path do diretório atual
+// __filename - nome do arquivo
+// require - função par usar modulos (tipo import do es6)
+// module - informações do modulo atual (arquivo)
+// process - informções sobre o ambiente onde o programa está rodando
+
+console.log(__dirname);
+
+setInterval(() => {
+    console.log("hello, world")
+}, 1000)
